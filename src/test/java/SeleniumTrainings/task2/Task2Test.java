562fceb181a2c1a2e6a3159be0ff186763e86bfb
gldadnri <denys.andriyanov@avid.com>
2021-03-02 15:24:56 +0200

    task 2 done

---
+++ src/test/java/SeleniumTrainings/task2/Task2Test.java
@@ -0,0 +1,127 @@
+package SeleniumTrainings.task2;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task2Test {
+
+    static WebDriver driver;
+
+    @BeforeAll
+    static void setUp()
+    { 
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    static void tearDown()
+    {
+        driver.quit();   
+    }
+
+    /**
+     * Opens page, enters user name and password, tries to log in
+     * Verifies log in is successful after 30 sec timeout
+     */
+    @Test 
+    void LogInToAdminPannelAndCheckTitle() 
+    {        
+        driver.get("http://158.101.173.161/admin/");
+        WebElement logInForm = driver.findElement(By.id("box-login"));
+
+        WebElement userName = logInForm.findElement(By.name("username"));
+        userName.clear();
+        userName.sendKeys("testadmin");
+
+        WebElement userPass = logInForm.findElement(By.name("password"));
+        userPass.clear();
+        userPass.sendKeys("R8MRDAYT_test");
+
+        driver.findElement(By.cssSelector("button.btn[type='submit']")).click();
+        new WebDriverWait(driver,30).until(ExpectedConditions.titleContains("Dashboard"));
+
+        // wait will already have failed, but the condition
+        // we are testing for is the page title here
+        Assertions.assertEquals("Dashboard | Selenium Test Store", driver.getTitle(), "Title does not match");
+    }
+
+    /**
+     * Opens each menu item and check presence of header element
+     */
+    @Test 
+    void OpenMenusAndCheckHeader() 
+    {        
+        //get menu items count
+        int mainMenusCount = driver.findElements(By.cssSelector("#box-apps-menu li.app")).size();
+
+        //cycle through all main menus
+        for (int mainMenuIndex = 0; mainMenuIndex <= mainMenusCount - 1; mainMenuIndex++)
+        {            
+            //get current menu object by it's index
+            WebElement menu =  driver.findElements(By.cssSelector("#box-apps-menu li.app")).get(mainMenuIndex); 
+
+            //store menu name
+            String menuItemName = menu.getAttribute("data-code"); 
+
+            //open menu
+            menu.click();
+            
+            //check that header object is present after main menu is opened
+            this.checkHeaderForMenuItemAndAssert(driver, menuItemName);
+         
+            //get menu id again, this time by it's data-code value
+            menu = driver.findElement(By.cssSelector("#box-apps-menu li.app[data-code='" + menuItemName + "']"));
+        
+            //get sub menu count for current main menu
+            int subMenusCount = driver.findElements(By.cssSelector("#box-apps-menu li.app[data-code='" + menuItemName + "'] li.doc")).size();
+
+            //cycle through all sub menus
+            for (int subMenuIndex = 0; subMenuIndex <= subMenusCount - 1; subMenuIndex++)
+            {
+                //get current sub menu object
+                WebElement subMenu =  driver.findElements(By.cssSelector("#box-apps-menu li.app[data-code='" + menuItemName + "'] li.doc")).get(subMenuIndex); 
+
+                //store menu name
+                String subMenuItemName = subMenu.getAttribute("data-code"); 
+
+                //open menu
+                subMenu.click();
+                
+                //check that header object is present after main menu is opened
+                this.checkHeaderForMenuItemAndAssert(driver, subMenuItemName);              
+            }
+
+        }
+
+
+    }
+
+
+    /**
+     * Checks for presence of element for given menu item and asserts in case not found
+     * 
+     * @param driver the Webdriver to use
+     * @param menuItem menuItem that is opened
+     */
+    public void checkHeaderForMenuItemAndAssert(WebDriver driver, String menuItem)
+    {
+        Assertions.assertFalse(driver.findElements(By.cssSelector("div.panel-heading")).size() < 0, "Header object was not found for '" + menuItem + "' menu");
+    }
+}
